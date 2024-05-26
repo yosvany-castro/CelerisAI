3d8a155f3af1e5de21f692109d535c364e5bc6ad
yosvany-castro <jorgeliasmh1998@gmail.com>
2024-05-26 03:39:48 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  React.createElement('h1', null, 'Hello, world!'),
+  document.getElementById('root')
+);
