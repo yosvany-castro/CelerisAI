459bbd83a149f20c65990aa0f7c6c4dc113b1b4d
yosvany-castro <jorgeliasmh1998@gmail.com>
2024-05-26 04:16:07 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production', // O 'development' si estás en modo de desarrollo
-  entry: './index.js',
+  mode: 'production', // Cambia a 'development' si estás en modo de desarrollo
+  entry: './src/index.js', // Asegúrate de que este archivo existe y es el punto de entrada de tu aplicación
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
